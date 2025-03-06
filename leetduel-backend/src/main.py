bf2fa18d28dc1c21d07619ac4ee5d87c070d4e0c
jeffreykim <jkjeffrey7@gmail.com>
2025-03-06 00:22:32 -0800

    Cleaned up code

--- leetduel-backend/src/main.py
+++ leetduel-backend/src/main.py
@@ -1,17 +1,20 @@
-import socketio
 import random
 import string
-from fastapi import FastAPI
-import uvicorn
-from fastapi.responses import JSONResponse
-from .submit import Problem
+
+import socketio
 import asyncio
+import uvicorn
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+from .submit import Problem
 from .database import SessionLocal
-from .crud import get_problem, get_count
+from .crud import get_problem
 
 from src.routes.problems import router as problems_router
 
-language_id = 100
+
 
 app = FastAPI()
 app.include_router(problems_router)
@@ -20,34 +23,39 @@ sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
 socket_app = socketio.ASGIApp(sio, app)
 
 parties = {}
+language_id = 100
 
 
-def get_random_problem(difficulty, problem_id=None):
+# <----------------- Helper functions ----------------->
+
+def get_random_problem(difficulty: list[bool], problem_id: int = None) -> dict:
     db = SessionLocal()
 
     try:
-        print(problem_id)
         problem = get_problem(db, difficulty, problem_id)
-        print(problem)
         return {"name": problem.problem_name, "description": problem.problem_description, "difficulty": problem.problem_difficulty, "test_cases": problem.test_cases, "function_signature": problem.function_signature}
+    
     finally:
         db.close()
 
 
-def generate_party_code():
+def generate_party_code() -> str:
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
 
-def all_players_passed(party_code):
+
+def all_players_passed(party_code: str) -> bool:
     for player in parties[party_code]["players"]:
         if not player["passed"]:
             return False
     return True
 
-def reset_players_passed(party_code):
+
+def reset_players_passed(party_code: str) -> None:
     for player in parties[party_code]["players"]:
         player["passed"] = False
 
-async def game_timeout(party_code, time_limit):
+
+async def game_timeout(party_code: str, time_limit: str) -> None:
     await asyncio.sleep(int(time_limit) * 60)
     if parties[party_code]["status"] == "in_progress":
         parties[party_code]["status"] = "finished"
@@ -56,8 +64,12 @@ async def game_timeout(party_code, time_limit):
         await asyncio.sleep(3)
         await sio.emit("game_over", room=party_code)
 
+
+
+# <----------------- Socket events ----------------->
+
 @sio.event
-async def create_party(sid, data):
+async def create_party(sid: str, data: dict) -> None:
     print(f"create_party event received from {sid}: {data}")
     party_code = generate_party_code()
     parties[party_code] = {
@@ -66,13 +78,12 @@ async def create_party(sid, data):
         "problem": None,
         "status": "waiting",
     }
-    print(party_code)
     await sio.emit("party_created", {"username": data["username"], "party_code": party_code}, to=sid)
     await sio.enter_room(sid, party_code)
 
 
 @sio.event
-async def join_party(sid, data):
+async def join_party(sid: str, data: dict) -> None:
     print(f"join_party event received from {sid}: {data}")
     party_code = data["party_code"]
     username = data["username"]
@@ -87,7 +98,7 @@ async def join_party(sid, data):
 
 
 @sio.event
-async def start_game(sid, data):
+async def start_game(sid: str, data: dict) -> None:
     print(f"start_game event received from {sid}: {data}")
     party_code = data["party_code"]
     difficulty = [data["easy"], data["medium"], data["hard"]]
@@ -106,8 +117,9 @@ async def start_game(sid, data):
     else:
         await sio.emit("error", {"message": "You are not the host"}, to=sid)
 
+
 @sio.event
-async def submit_code(sid, data):
+async def submit_code(sid: str, data: dict) -> None:
     print(f"submit_code event received from {sid}: {data}")
     party_code = data["party_code"]
     code = data["code"]
@@ -123,8 +135,8 @@ async def submit_code(sid, data):
     else:
         message_to_client = r["status"] + ", " + str(r["passed test cases"]) + "/" + str(r["total test cases"]) + " test cases in " + str(r["time"]) + " seconds."
         message_to_room = data["username"] + " passed " + str(r["passed test cases"]) + "/" + str(r["total test cases"]) + " test cases in " + str(r["time"]) + " seconds."
-
-    print("message " + message_to_client)
+        if r["failed_test"] is not None:
+            message_to_client += "\n" + r["failed_test"]
 
     if r["status"] == "Accepted":
         for player in parties[party_code]["players"]:
@@ -135,15 +147,15 @@ async def submit_code(sid, data):
     await sio.emit("player_submit", {"message": message_to_room}, room=party_code)
 
     if all_players_passed(party_code):
-        print("All players passed!")
         parties[party_code]["status"] = "finished"
         reset_players_passed(party_code)
         await sio.emit("game_over_message", {"message": "All players passed! Game over."}, room=party_code)
         await asyncio.sleep(3)
         await sio.emit("game_over", room=party_code)
 
+
 @sio.event
-async def chat_message(sid, data):
+async def chat_message(sid: str, data: dict) -> None:
     print(f"chat_message event received from {sid}: {data}")
     party_code = data["party_code"]
     message = data["message"]
@@ -151,8 +163,9 @@ async def chat_message(sid, data):
 
     await sio.emit("message_received", {"username": username, "message": message}, room=party_code)
 
+
 @sio.event
-async def leave_party(sid, data):
+async def leave_party(sid: str, data: dict) -> None:
     print(f"leave_party event received from {sid}: {data}")
     party_code = data["party_code"]
     username = data["username"]
@@ -174,6 +187,7 @@ async def leave_party(sid, data):
 @app.get("/")
 async def read_root():
     return JSONResponse({"message": "Server is running"})
+
 
 @app.get("/parties")
 async def read_root():
--- leetduel-backend/src/submit.py
+++ leetduel-backend/src/submit.py
@@ -2,19 +2,17 @@ import json
 import subprocess
 
 
-with open("problems.json", "r") as f:
-    problems = json.load(f)
-    
 
 class Problem:
-    def __init__(self, language_id, problem):
+
+
+    def __init__(self, language_id: int, problem: dict):
         self.language_id = language_id
         self.problem = problem
         self.stdinput = json.dumps([json.loads(test_case["input"]) for test_case in problem["test_cases"]])
 
 
-    def submit_code(self, code: str, timeout: int = 10) -> str:
-        """Executes code with a timeout to prevent infinite loops."""
+    def submit_code(self, code: str, timeout: int = 5) -> str:
         code = "import sys\nimport json\n" + code + """
 input_data = sys.stdin.read().strip()
 test_cases = json.loads(input_data)
@@ -22,7 +20,7 @@ results = [run(*args) for args in test_cases]
 for result in results:
     print(result)
         """
-        print(self.stdinput)
+
         try:
             result = subprocess.run(
                 ["docker", "run", "-i", "--rm", "--memory=100m", "--cpu-shares=50", "code-runner", "python3", "-c", code],
@@ -34,42 +32,39 @@ for result in results:
             if result.returncode != 0:
                 return {"message": result.stderr, "status": "Failed"}
             
-            print(result.stdout)
             return self.check_test_cases(result.stdout, "N/A")
 
         except subprocess.TimeoutExpired:
             return {"message": "Time limit exceeded", "status": "Failed"}
+        
         except Exception as e:
             return {"message": str(e), "status": "Failed"}
         
 
-    def check_test_cases(self, data, time):
+    def check_test_cases(self, data: dict, time: int) -> dict:
         test_cases = self.problem["test_cases"]
         if not data:
-            return "No output"
+            return {"message": "No output", "status": "Failed"}
+        
         data = data.split("\n")[:-1]
-        print(test_cases, data)
         count = 0
+        failed_index = -1
+
         r = {"status": "Accepted", "total test cases": len(test_cases), "time": time}
 
         for i in range(len(data)):
             if data[i] != test_cases[i]["output"]:
                 r["status"] = "Failed"
+                if failed_index == -1:
+                    failed_index = i
 
-                if "test_case" not in r:
-                    r["test_case"] = i
                 continue
 
             count += 1
         
         r["passed test cases"] = count
-        print(r["status"])
-        return r
-    
-    
-    def get_problem_title(self):
-        return self.problem["title"]
-    
+        
+        if failed_index != -1:
+            r["failed_test"] = "Input: " + test_cases[failed_index]["input"] + "\nExpected " + test_cases[failed_index]["output"] + ", got " + data[failed_index]
 
-    def get_problem_description(self):
-        return self.problem["description"]
+        return r
